e0f97d26c9824c60024c07bcd420a39e22664717
Ivan Brazhnikov <samsungpc239@gmail.com>
2016-03-13 19:09:03 +0300

    Initial Commit

---
+++ IBSwiftToolKit/IBSwiftToolKit.h
@@ -0,0 +1,19 @@
+//
+//  IBSwiftToolKit.h
+//  IBSwiftToolKit
+//
+//  Created by Ivan Brazhnikov on 13.03.16.
+//  Copyright © 2016 Ivan Brazhnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBSwiftToolKit.
+FOUNDATION_EXPORT double IBSwiftToolKitVersionNumber;
+
+//! Project version string for IBSwiftToolKit.
+FOUNDATION_EXPORT const unsigned char IBSwiftToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBSwiftToolKit/PublicHeader.h>
+
+
