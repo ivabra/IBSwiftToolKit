f947f522a9e165f9e73820e8e0abb69f4260a06d
Ivan Brazhnikov <samsungpc239@gmail.com>
2016-03-14 13:51:12 +0300

    0.0.4

---
+++ IBSwiftToolKit/Classes/Operators/CommonOperators.swift
@@ -0,0 +1,27 @@
+//
+//  CommonOperators.swift
+//  IBSwiftToolKit
+//
+//  Created by Ivan Brazhnikov on 14.03.16.
+//  Copyright © 2016 Ivan Brazhnikov. All rights reserved.
+//
+
+infix operator =! {}
+
+public func =! <T>(inout left: T, right: Any) {
+    left = right as! T
+}
+
+
+infix operator =? {}
+
+public func =? <T>(inout left: T, right: Any?) {
+    if let right = right as? T {
+        left = right
+    }
+}
+
+public func =? <T>(inout left: T!, right: Any?) {
+    left = right as? T
+}
+
