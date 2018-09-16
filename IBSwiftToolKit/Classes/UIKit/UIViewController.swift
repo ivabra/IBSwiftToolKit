e470cf1d635a84d388ca87ba6d79eb89f3780554
Ivan Brazhnikov <samsungpc239@gmail.com>
2018-09-16 04:09:21 +0300

    removed vc methods

--- IBSwiftToolKit/Classes/UIKit/UIViewController.swift
+++ IBSwiftToolKit/Classes/UIKit/UIViewController.swift
@@ -2,18 +2,6 @@ import UIKit
 
 extension UIViewController {
 
-public func addChildViewController(_ controller: UIViewController, toView view: UIView) {
-    addChild(controller)
-    controller.view.frame = view.bounds
-    view.addSubview(controller.view)
-    controller.didMove(toParent: self)
-  }
-
-  public func removeFromParentViewControllerCompletely() {
-    view.removeFromSuperview()
-    removeFromParent()
-  }
-
   /// Find the nearest parent view Controller with specified `class`.
   ///- Parameter class: Class of desired parent view controller.
   ///- Returns: Nearest parent view controller with specified `class`, or `nil`.
